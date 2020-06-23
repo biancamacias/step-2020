f114670a1388730b600cd8dbfcf5d5fb84150c0e
Bianca Macias <bmac@biancamacias.com>
2020-06-23 22:04:21 +0000

    added file to practice using libraries in java

---
+++ personal-practice/library.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.util.Scanner;
+import java.util.concurrent.Future;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
+public class library {
+
+    public static void main(String[] args) {
+        double power = Math.pow(5, 3);
+        System.out.println(power);
+
+        double squareRoot = Math.sqrt(64);
+        System.out.println(squareRoot);
+
+        Random rand = new Random();
+        int randomNumber = rand.nextInt();
+        int randomNumberWithBound = rand.nextInt(10);
+
+        System.out.println(randomNumber);
+        System.out.println(randomNumberWithBound);
+    }
+}
