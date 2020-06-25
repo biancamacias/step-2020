edddaa090806a21b429ad6344c4a4e507ce5516d
Bianca Macias <bmac@biancamacias.com>
2020-06-25 22:32:06 +0000

    modified script.js file to make a random quote generator

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,20 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random quote to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+function addRandomQuote() {
+  const quotes =
+      ['The secret of getting ahead is getting started.', 'What you do today can improve all your tomorrows',
+       'A single sunbeam is enough to drive away many shadows',
+       'It is never too late to be what you might have been.', 
+       'A true friend never gets in your way unless you happen to be going down.'];
+
+  // Pick a random quote.
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const quoteContainer = document.getElementById('quote-container');
+  quoteContainer.innerText = quote;
 }
