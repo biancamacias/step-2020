40fc7b54966129407970587ba9bee322d31f9eaf
Bianca Macias <bmac@biancamacias.com>
2020-07-06 20:21:40 +0000

    updated content type in DataServelet.java to be text/plain instead of text/html

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -26,7 +26,7 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
+    response.setContentType("text/plain;");
     response.getWriter().println("Hello Bianca!");
   }
 }
