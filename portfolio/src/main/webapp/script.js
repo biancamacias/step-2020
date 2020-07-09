9116d14e655ccb82e450dc7adf95bd33912a11be
Bianca Macias <bmac@biancamacias.com>
2020-07-09 18:56:06 +0000

    added back fetchComments() and createListElement()

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -53,3 +53,22 @@ function addName(name) {
     const nameDiv = document.getElementById('name-container');
     nameDiv.innerText = name;
 }
+
+function fetchComments() {
+    fetch('/data').then(response => response.json()).then((comments) => {
+        console.log('Fetching comments...');
+        console.log(comments);
+        const commentListElement = document.getElementById('comment-container');
+        commentListElement.innerHTML = '';
+        for (index = 0; index < comments.length; index ++) {
+            commentListElement.appendChild(
+                createListElement(comments[index]));
+        }
+    });
+}
+
+function createListElement(text) {
+    const liElement = document.createElement('li');
+    liElement.innerText = text;
+    return liElement;
+}
