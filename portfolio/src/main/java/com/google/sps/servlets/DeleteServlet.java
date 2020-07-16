96bf07ddbb0d202d26de153cc64968517dd27440
Bianca Macias <bmac@biancamacias.com>
2020-07-16 17:18:59 +0000

    made datastore variable private final

--- portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
@@ -21,13 +21,11 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/delete-data")
 public class DeleteServlet extends HttpServlet {
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // TO DO(biancamacias): add data structure of keys and add while loop that deletes keys from that data structure
-        Query query = new Query(COMMENT_TABLE_NAME).addSort(TIMESTAMP_COLUMN_NAME, SortDirection.DESCENDING);
-        PreparedQuery results = datastore.prepare(query);
 
         response.sendRedirect("/index.html");
     }
