3fd1705ee2958bb8a48a7b129e190e086796edb6
Bianca Macias <bmac@biancamacias.com>
2020-07-21 18:33:52 +0000

    set up greetet test java file in portfolio's test directory

---
+++ portfolio/src/test/java/com/google/sps/GreeterTest.java
@@ -0,0 +1,17 @@
+package com.google.sps;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class GreeterTest {
+  
+}
