b89bb8e9266fe93601fc14966f939664b0f8b9ad
Bianca Macias <bmac@biancamacias.com>
2020-07-23 22:33:41 +0000

    added Mockito test rule

--- portfolio/src/test/java/com/google/sps/NameTest.java
+++ portfolio/src/test/java/com/google/sps/NameTest.java
@@ -3,43 +3,33 @@ package com.google.sps.servlets;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.io.IOException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletOutputStream;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 @RunWith(JUnit4.class)
 public final class NameTest {
 
   private final DataServlet servlet = new DataServlet();
 
-  @Mock
-  private final HttpServletRequest request;
-  @Mock
-  private final HttpServletResponse response;
-  @Mock
-  private final ServletOutputStream outputStream;
+  @Rule public final MockitoRule mockito = MockitoJUnit.rule();
+
+  @Mock private final HttpServletRequest request;
+  @Mock private final HttpServletResponse response;
+  @Mock private final ServletOutputStream outputStream;
 
   @Test
   public void getMyName() throws IOException {
     when(response.getOutputStream()).thenReturn(outputStream);
     servlet.doGet(request, response);
-    
+
     verify(response).setContentType("plain/text");
     verify(outputStream).println("Hello Bianca!");
   }
