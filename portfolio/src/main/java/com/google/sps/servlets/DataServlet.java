240479b12a81cc4efdbed70bb092780f34dd447b
Bianca Macias <bmac@biancamacias.com>
2020-07-23 18:27:34 +0000

    ran formatter through DataServlet.java and DeleteServlet.java

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -18,7 +18,6 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.FetchOptions.Builder;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
@@ -34,41 +33,42 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-    static final String COMMENT_TABLE_NAME = "Comment";
-    static final String COMMENT_COLUMN_NAME = "comment";
-    static final String TIMESTAMP_COLUMN_NAME = "submit_time";
-    private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+  static final String COMMENT_TABLE_NAME = "Comment";
+  static final String COMMENT_COLUMN_NAME = "comment";
+  static final String TIMESTAMP_COLUMN_NAME = "submit_time";
+  private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      Query query = new Query(COMMENT_TABLE_NAME).addSort(TIMESTAMP_COLUMN_NAME, SortDirection.DESCENDING);
-      PreparedQuery results = datastore.prepare(query);
-      List<String> commentsList = new ArrayList<String>();
-      List<Entity> limitedResults = results.asList(FetchOptions.Builder.withLimit(2));
+    Query query =
+        new Query(COMMENT_TABLE_NAME).addSort(TIMESTAMP_COLUMN_NAME, SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+    List<String> commentsList = new ArrayList<String>();
+    List<Entity> limitedResults = results.asList(FetchOptions.Builder.withLimit(2));
 
-      for (Entity entity : limitedResults) {
-          String commentInQuery = (String) entity.getProperty(COMMENT_COLUMN_NAME);
-          commentsList.add(commentInQuery);
-      }
-    
-      response.setContentType("application/json");
-      String json = convertToJsonUsingGson(commentsList);
-      response.getWriter().println(json);
+    for (Entity entity : limitedResults) {
+      String commentInQuery = (String) entity.getProperty(COMMENT_COLUMN_NAME);
+      commentsList.add(commentInQuery);
+    }
+
+    response.setContentType("application/json");
+    String json = convertToJsonUsingGson(commentsList);
+    response.getWriter().println(json);
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      // Gets comment input from <form> by id selector
-      String comment = request.getParameter("comment-input");
-      long submit_time = System.currentTimeMillis();
-      
-      Entity commentEntity = new Entity(COMMENT_TABLE_NAME);
-      commentEntity.setProperty(COMMENT_COLUMN_NAME, comment);
-      commentEntity.setProperty(TIMESTAMP_COLUMN_NAME, submit_time);
-      
-      datastore.put(commentEntity);
-      
-      response.sendRedirect("/index.html");
+    // Gets comment input from <form> by id selector
+    String comment = request.getParameter("comment-input");
+    long submit_time = System.currentTimeMillis();
+
+    Entity commentEntity = new Entity(COMMENT_TABLE_NAME);
+    commentEntity.setProperty(COMMENT_COLUMN_NAME, comment);
+    commentEntity.setProperty(TIMESTAMP_COLUMN_NAME, submit_time);
+
+    datastore.put(commentEntity);
+
+    response.sendRedirect("/index.html");
   }
 
   private static String convertToJsonUsingGson(List<String> commentsToConvert) {
